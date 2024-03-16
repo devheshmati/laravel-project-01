13e4c7e41aa35af303b5d50e68d7949a2a1d0846
womoboy <reza2boyce@gmail.com>
2024-03-16 22:03:08 +0330

    add contact us page

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-});
+Route::get(
+    '/', function () {
+        return view('pages.home');
+    }
+);
 
-Route::get('/about/about', function () {
-    return view('pages.about');
-});
+Route::get(
+    '/about', function () {
+        return view('pages.about');
+    }
+);
+
+Route::get(
+    '/contact', function () {
+        return view('pages.contact');
+    }
+);
