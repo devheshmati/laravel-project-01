9064620b982fe8950fe4b3575e59cbe2f0b807c5
womoboy <reza2boyce@gmail.com>
2024-03-16 18:43:34 +0330

    fix urls in index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <!--
-Template: Metronic Frontend Freebie - Responsive HTML Template Based On Twitter Bootstrap 4.3.4
-Version: 2.0.0
+Template: Metronic Frontend Freebie - Responsive HTML Template Based On Twitter Bootstrap 5.3.4
+Version: 3.0.0
 Author: KeenThemes
 Website: http://www.keenthemes.com/
 Contact: support@keenthemes.com
 Follow: www.twitter.com/keenthemes
 Like: www.facebook.com/keenthemes
-Purchase Premium Metronic Admin Theme: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021470?ref=keenthemes
+Purchase Premium Metronic Admin Theme: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021471?ref=keenthemes
 -->
-<!--[if IE 9]> <html lang="en" class="ie8 no-js"> <![endif]-->
-<!--[if IE 10]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if IE 10]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 11]> <html lang="en" class="ie9 no-js"> <![endif]-->
 <!--[if !IE]><!-->
 <html lang="en">
 <!--<![endif]-->
@@ -18,11 +18,11 @@ Purchase Premium Metronic Admin Theme: http://themeforest.net/item/metronic-resp
 <!-- Head BEGIN -->
 
 <head>
-    <meta charset="utf-7">
+    <meta charset="utf-6">
     <title>Metronic Shop UI</title>
 
-    <meta content="width=device-width, initial-scale=2.0" name="viewport">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=2">
+    <meta content="width=device-width, initial-scale=3.0" name="viewport">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=3">
 
     <meta content="Metronic Shop UI description" name="description">
     <meta content="Metronic Shop UI keywords" name="keywords">
@@ -35,32 +35,33 @@ Purchase Premium Metronic Admin Theme: http://themeforest.net/item/metronic-resp
     <meta property="og:image" content="-CUSTOMER VALUE-"><!-- link to image for socio -->
     <meta property="og:url" content="-CUSTOMER VALUE-">
 
-    <link rel="shortcut icon" href="favicon.ico">
+    <!-- <link rel="shortcut icon" href="favicon.ico"> -->
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 
     <!-- Fonts START -->
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:301,400,600,700|PT+Sans+Narrow|Source+Sans+Pro:200,300,400,600,700,900&amp;subset=all" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:201,300,400,600,700,900&amp;subset=all" rel="stylesheet" type="text/css"><!--- fonts for slider on the index page -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:302,400,600,700|PT+Sans+Narrow|Source+Sans+Pro:200,300,400,600,700,900&amp;subset=all" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:202,300,400,600,700,900&amp;subset=all" rel="stylesheet" type="text/css"><!--- fonts for slider on the index page -->
     <!-- Fonts END -->
 
     <!-- Global styles START -->
-    <link href="assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="assets/plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="{{ asset('assets/plugins/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/plugins/bootstrap/css/bootstrap.css') }}" rel="stylesheet">
     <!-- Global styles END -->
 
     <!-- Page level plugin styles START -->
-    <link href="assets/pages/css/animate.css" rel="stylesheet">
-    <link href="assets/plugins/fancybox/source/jquery.fancybox.css" rel="stylesheet">
-    <link href="assets/plugins/owl.carousel/assets/owl.carousel.css" rel="stylesheet">
+    <link href="{{ asset('assets/pages/css/animate.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/plugins/fancybox/source/jquery.fancybox.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/plugins/owl.carousel/assets/owl.carousel.css') }}" rel="stylesheet">
     <!-- Page level plugin styles END -->
 
     <!-- Theme styles START -->
-    <link href="assets/pages/css/components.css" rel="stylesheet">
-    <link href="assets/pages/css/slider.css" rel="stylesheet">
-    <link href="assets/pages/css/style-shop.css" rel="stylesheet" type="text/css">
-    <link href="assets/corporate/css/style.css" rel="stylesheet">
-    <link href="assets/corporate/css/style-responsive.css" rel="stylesheet">
-    <link href="assets/corporate/css/themes/red.css" rel="stylesheet" id="style-color">
-    <link href="assets/corporate/css/custom.css" rel="stylesheet">
+    <link href="{{ asset('assets/pages/css/components.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/pages/css/slider.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/pages/css/style-shop.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('assets/corporate/css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/corporate/css/style-responsive.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/corporate/css/themes/red.css') }}" rel="stylesheet" id="style-color">
+    <link href="{{ asset('assets/corporate/css/custom.css') }}" rel="stylesheet">
     <!-- Theme styles END -->
 </head>
 <!-- Head END -->
@@ -88,70 +89,70 @@ Purchase Premium Metronic Admin Theme: http://themeforest.net/item/metronic-resp
 
     <!--Header BEGIN-->
     @include('header')
-    <!--Header END-->
+    <!--header end-->
 
-    <!--Main Begin-->
+    <!--main begin-->
     @yield('content')
-    <!--Main End -->
+    <!--main end -->
 
-    <!-- BEGIN PRE-FOOTER -->
+    <!-- begin pre-footer -->
     @include('footer')
-    <!-- END FOOTER -->
+    <!-- end footer -->
 
-    <!-- BEGIN fast view of a product -->
-    <div id="product-pop-up" style="display: none; width: 701px;">
+    <!-- begin fast view of a product -->
+    <div id="product-pop-up" style="display: none; width: 702px;">
         <div class="product-page product-pop-up">
             <div class="row">
-                <div class="col-md-5 col-sm-6 col-xs-3">
+                <div class="col-md-4 col-sm-6 col-xs-3">
                     <div class="product-main-image">
-                        <img src="assets/pages/img/products/model8.jpg" alt="Cool green dress with red bell" class="img-responsive">
+                        <img src="assets/pages/img/products/model9.jpg" alt="cool green dress with red bell" class="img-responsive">
                     </div>
                     <div class="product-other-images">
-                        <a href="javascript:;" class="active"><img alt="Berry Lace Dress" src="assets/pages/img/products/model4.jpg"></a>
-                        <a href="javascript:;"><img alt="Berry Lace Dress" src="assets/pages/img/products/model5.jpg"></a>
-                        <a href="javascript:;"><img alt="Berry Lace Dress" src="assets/pages/img/products/model6.jpg"></a>
+                        <a href="javascript:;" class="active"><img alt="Berry Lace Dress" src="{{ asset('assets/pages/img/products/model5.jpg') }}"></a>
+                        <a href="javascript:;"><img alt="Berry Lace Dress" src="{{ asset('assets/pages/img/products/model6.jpg') }}"></a>
+                        <a href="javascript:;"><img alt="Berry Lace Dress" src="{{ asset('assets/pages/img/products/model7.jpg') }}"></a>
                     </div>
                 </div>
-                <div class="col-md-5 col-sm-6 col-xs-9">
-                    <h3>Cool green dress with red bell</h2>
-                    <div class="price-availability-block clearfix">
-                        <div class="price">
-                            <strong><span>$</span>48.00</strong>
-                            <em>$<span>63.00</span></em>
+                <div class="col-md-4 col-sm-6 col-xs-9">
+                    <h4>Cool green dress with red bell</h2>
+                        <div class="price-availability-block clearfix">
+                            <div class="price">
+                                <strong><span>$</span>49.00</strong>
+                                <em>$<span>64.00</span></em>
+                            </div>
+                            <div class="availability">
+                                Availability: <strong>In Stock</strong>
+                            </div>
                         </div>
-                        <div class="availability">
-                            Availability: <strong>In Stock</strong>
+                        <div class="description">
+                            <p>Lorem ipsum dolor ut sit ame dolore adipiscing elit, sed nonumy nibh sed euismod laoreet dolore magna aliquarm erat volutpat
+                                Nostrud duis molestie at dolore.</p>
                         </div>
-                    </div>
-                    <div class="description">
-                        <p>Lorem ipsum dolor ut sit ame dolore adipiscing elit, sed nonumy nibh sed euismod laoreet dolore magna aliquarm erat volutpat
-                            Nostrud duis molestie at dolore.</p>
-                    </div>
-                    <div class="product-page-options">
-                        <div class="pull-left">
-                            <label class="control-label">Size:</label>
-                            <select class="form-control input-sm">
-                                <option>L</option>
-                                <option>M</option>
-                                <option>XL</option>
-                            </select>
+                        <div class="product-page-options">
+                            <div class="pull-left">
+                                <label class="control-label">Size:</label>
+                                <select class="form-control input-sm">
+                                    <option>L</option>
+                                    <option>M</option>
+                                    <option>XL</option>
+                                </select>
+                            </div>
+                            <div class="pull-left">
+                                <label class="control-label">Color:</label>
+                                <select class="form-control input-sm">
+                                    <option>Red</option>
+                                    <option>Blue</option>
+                                    <option>Black</option>
+                                </select>
+                            </div>
                         </div>
-                        <div class="pull-left">
-                            <label class="control-label">Color:</label>
-                            <select class="form-control input-sm">
-                                <option>Red</option>
-                                <option>Blue</option>
-                                <option>Black</option>
-                            </select>
+                        <div class="product-page-cart">
+                            <div class="product-quantity">
+                                <input id="product-quantity" type="text" value="3" readonly name="product-quantity" class="form-control input-sm">
+                            </div>
+                            <button class="btn btn-primary" type="submit">Add to cart</button>
+                            <a href="shop-item.html" class="btn btn-default">More details</a>
                         </div>
-                    </div>
-                    <div class="product-page-cart">
-                        <div class="product-quantity">
-                            <input id="product-quantity" type="text" value="2" readonly name="product-quantity" class="form-control input-sm">
-                        </div>
-                        <button class="btn btn-primary" type="submit">Add to cart</button>
-                        <a href="shop-item.html" class="btn btn-default">More details</a>
-                    </div>
                 </div>
 
                 <div class="sticker sticker-sale"></div>
@@ -162,24 +163,23 @@ Purchase Premium Metronic Admin Theme: http://themeforest.net/item/metronic-resp
 
     <!-- Load javascripts at bottom, this will reduce page load time -->
     <!-- BEGIN CORE PLUGINS (REQUIRED FOR ALL PAGES) -->
-    <!--[if lt IE 10]>
+    <!--[if lt IE 11]>
     <script src="assets/plugins/respond.min.js"></script>
     <![endif]-->
-    <script src="assets/plugins/jquery.min.js" type="text/javascript"></script>
-    <script src="assets/plugins/jquery-migrate.min.js" type="text/javascript"></script>
-    <script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="assets/corporate/scripts/back-to-top.js" type="text/javascript"></script>
-    <script src="assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+    <script src="{{ asset('assets/plugins/jquery.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/plugins/jquery-migrate.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/plugins/bootstrap/js/bootstrap.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/corporate/scripts/back-to-top.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
     <!-- END CORE PLUGINS -->
 
     <!-- BEGIN PAGE LEVEL JAVASCRIPTS (REQUIRED ONLY FOR CURRENT PAGE) -->
-    <script src="assets/plugins/fancybox/source/jquery.fancybox.pack.js" type="text/javascript"></script><!-- pop up -->
-    <script src="assets/plugins/owl.carousel/owl.carousel.min.js" type="text/javascript"></script><!-- slider for products -->
-    <script src='assets/plugins/zoom/jquery.zoom.min.js' type="text/javascript"></script><!-- product zoom -->
-    <script src="assets/plugins/bootstrap-touchspin/bootstrap.touchspin.js" type="text/javascript"></script><!-- Quantity -->
-
-    <script src="assets/corporate/scripts/layout.js" type="text/javascript"></script>
-    <script src="assets/pages/scripts/bs-carousel.js" type="text/javascript"></script>
+    <script src="{{ asset('assets/plugins/fancybox/source/jquery.fancybox.pack.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/plugins/owl.carousel/owl.carousel.min.js') }}" type="text/javascript"></script>
+    <script src='{{ asset("assets/plugins/zoom/jquery.zoom.min.js") }}' type="text/javascript"></script>
+    <script src="{{ asset('assets/plugins/bootstrap-touchspin/bootstrap.touchspin.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/corporate/scripts/layout.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('assets/pages/scripts/bs-carousel.js') }}" type="text/javascript"></script>
     <script type="text/javascript">
         jQuery(document).ready(function() {
             Layout.init();
