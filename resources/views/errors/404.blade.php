01171d8c696123409fae404d527fd67df471aaa0
womoboy <reza2boyce@gmail.com>
2024-03-16 17:54:57 +0330

    first commit

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,9 @@
+@extends('index')
+
+@section('content')
+<main class="bg-dark text-light min-h-100vh flex flex-col justify-center align-center">
+    <h1 class="text-center h1">404 Error Happend!</h1>
+    <p class="text-center">This page is not found</p>
+    <a href='/' class="h4 btn btn-danger">Go Home</a>
+</main>
+@endsection
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,8 @@
+@extends('index')
+
+@section('content')
+<main class="bg-dark text-light min-h-100vh flex flex-col justify-center align-center">
+    <h1 class="text-center">404 Error Happend!</h1>
+    <p class="text-center">This page is not found</p>
+</main>
+@endsection
